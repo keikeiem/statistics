b1303a17ca9c56fa94f0294f98d22ebc83546bb1
kimkm <keikeiem@aimtog.co.kr>
2019-01-10 05:31:00 +0900

    job ongoing

--- modules/matrix.js
+++ modules/matrix.js
@@ -41,8 +41,19 @@
         }
     };
 
+	/**
+	* each index is labeled on computing.
+	* 				not real mathematics
+	* 			(o) 0th row - 1st column
+	* 			(x) 1th row - 2th column
+	*/
     jsStatisticsAPI.computeMatrixMinor = function(matrix, rowIdx, columnIdx) {
-
+		var minor = JSON.parse(JSON.stringify(matrix));
+		minor.splice(rowIdx, 1);
+		minor.forEach(function(row) {
+			row.splice(columnIdx, 1);
+		});
+		return minor;
     };
 
     jsStatisticsAPI.computeMatrixDeterminant = function(matrix) {
@@ -50,21 +61,26 @@
             console.log('the matrix is not square.');
         }
 
-        // for (var i = 0; i < matrix.length; i++) {
-        //
-        // }
-        //
-        // var submatrix = [];
-        // if (matrix.length < 3) {
-        //     return (matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0]);
-        // } else {
-        //     for (var i = 1; i < matrix.length; i++) {
-        //         submatrix.push(matrix[i].slice(1))
-        //     }
-        //     submatrix.push()
-        // }
-        //
-        // return matrix[0][0] * this.computeMatrixDeterminant(submatrix);
+
+		var result = [];
+		for (var i = 0; i < matrix.length; i++) {
+			var submatrix = JSON.parse(JSON.stringify(matrix));
+			var multiplier = (0 + i) % 2 ? (-1 * matrix[0][i]) : matrix[0][i];
+			while (submatrix.length > 2) {
+				submatrix = this.computeMatrixMinor(submatrix, 0, i);
+				multiplier = multiplier * submatrix[0][0];
+			}
+			console.log(multiplier);
+			result.push(multiplier);
+
+
+			// if (submatrix.length > 1) {
+			// 	console.log(matrix);
+			// 	console.log('test : ', matrix[0][i] * this.computeMatrixDeterminant(submatrix))
+			// 	return matrix[0][i] * this.computeMatrixDeterminant(submatrix);
+			// }
+		}
+		console.log(result);
     };
 
 }(jsStatistics.API));
